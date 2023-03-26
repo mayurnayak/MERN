84523be54d8a70556819aff423f75770efaf16ef
Mayur Nayak <nayak.mayur22@gmail.com>
2023-03-26 13:32:55 -0400

    coin-flipping

---
+++ week4/promises.js
@@ -0,0 +1,34 @@
+
+function tossCoin() {
+    return Math.random() > 0.5 ? "heads" : "tails";
+}
+
+function fiveHeads() {
+    return new Promise((resolve, reject) => {
+        let headsCount = 0;
+        let attempts = 0;
+        let maxAttempts = 100;
+
+        while (headsCount < 5 && attempts <= maxAttempts) {
+            attempts++;
+            let result = tossCoin();
+            console.log(`${result} was flipped`);
+            if (result === "heads") {
+                headsCount++;
+            } else {
+                headsCount = 0;
+            }
+        }
+        if (attempts <= maxAttempts) {
+            resolve(`Number of attempts: ${attempts}, to flip five heads`);
+        } else {
+            reject(`${maxAttempts} attempts reached without heads.`);
+        }
+    });
+}
+
+fiveHeads()
+    .then((res) => console.log(res))
+    .catch((err) => console.log(err));
+
+console.log("When does this run now?");
